bf4205d223c0941e025d94228dede30a609a81ea
int-het-darji <het.darji@intellisolar.in>
2026-02-13 19:11:27 +0530

    added sidebar and create project

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,10 +1,17 @@
 import Sidebar from "../components/sidebar/Sidebar";
 import { Outlet } from "react-router-dom";
+import { adminMenu, userMenu } from "../conifg/menu.config";
 
 export default function MainLayout() {
+
+  // simulate login user
+  const user = { role: "admin" };
+
+  const links = user.role === "admin" ? adminMenu : userMenu;
+
   return (
     <div className="flex">
-      <Sidebar />
+      <Sidebar links={links} />
 
       <main className="flex-1 bg-gray-50 min-h-screen p-6">
         <Outlet />
--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Sidebar from "../components/sidebar/Sidebar";
+import Project from "../pages/project";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route path="/" element={<Sidebar />} />
         {/* <Route path="/" element={<Sidebar />} /> */}
+        {/* <Route path="/" element={<Sidebar />} /> */}
+        <Route path="/users" element={<Project />} />
+        <Route path="/projects" element={<Project />} />
       </Route>
     </Routes>
   );
