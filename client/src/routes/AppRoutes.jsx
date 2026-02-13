0d5073a8a1e95eaa8957fdd21d6341baba6cbe2a
int-dev-koyani <dev.koyani@intellisolar.in>
2026-02-13 19:14:53 +0530

    Added Create User Section

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Sidebar from "../components/sidebar/Sidebar";
+import Users from "../pages/Users";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route path="/" element={<Sidebar />} />
+        <Route path="/" element={<Users />} />
         {/* <Route path="/" element={<Sidebar />} /> */}
       </Route>
     </Routes>
